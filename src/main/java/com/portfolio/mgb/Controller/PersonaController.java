f57d8e1036beaa23fb0565a68607a6337224d736
RocioGuiral <127322629+RocioGuiral@users.noreply.github.com>
2023-04-27 16:31:53 -0300

    Update PersonaController.java

--- src/main/java/com/portfolio/mgb/Controller/PersonaController.java
+++ src/main/java/com/portfolio/mgb/Controller/PersonaController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")
-@CrossOrigin(origins = {"https://frontend-6a9e1.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"https://frontend-6a9e1.web.app"})
 public class PersonaController {
     @Autowired
     ImpPersonaService personaService;
