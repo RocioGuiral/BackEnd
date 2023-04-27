fdea552b670490e9286db291078ae4a78749a1b9
RocioGuiral <127322629+RocioGuiral@users.noreply.github.com>
2023-04-27 16:29:48 -0300

    Update CEducacion.java

--- src/main/java/com/portfolio/mgb/Controller/CEducacion.java
+++ src/main/java/com/portfolio/mgb/Controller/CEducacion.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = {"https://frontend-6a9e1.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"https://frontend-6a9e1.web.app"})
 
 public class CEducacion {
     @Autowired
