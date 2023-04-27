545dce9f74778045261c4eb3e1e97ea124df6c27
RocioGuiral <127322629+RocioGuiral@users.noreply.github.com>
2023-04-27 16:31:14 -0300

    Update CHys.java

--- src/main/java/com/portfolio/mgb/Controller/CHys.java
+++ src/main/java/com/portfolio/mgb/Controller/CHys.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin(origins = {"https://frontend-6a9e1.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"https://frontend-6a9e1.web.app"})
 @RequestMapping("/skill")
 public class CHys {
 
